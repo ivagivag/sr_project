2eda96390d9a5d8c59660312ca433a6339113af4
iva <iva@borabora.localdomain>
2022-04-10 16:26:21 +0300

    Fixed staticfils

--- srproj/srproj/urls.py
+++ srproj/srproj/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from srproj.main.views import display_main_page
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,7 +15,7 @@ urlpatterns = [
     path('', display_main_page, name='main page'),
     # path('faq/', TemplateView.as_view(template_name='base/faq_public.html'), name='faq'),
     # path('contacts/', TemplateView.as_view(template_name='base/contacts_public.html'), name='contacts'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = 'srproj.main.views.resource_not_found'
 handler403 = 'srproj.main.views.forbidden'
