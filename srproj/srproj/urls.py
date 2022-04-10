4569599a74985173dfc79ed63c4b4304f68394e8
iva <iva@borabora.localdomain>
2022-04-10 16:50:37 +0300

    css folders uploaded

--- srproj/srproj/urls.py
+++ srproj/srproj/urls.py
@@ -15,7 +15,9 @@ urlpatterns = [
     path('', display_main_page, name='main page'),
     # path('faq/', TemplateView.as_view(template_name='base/faq_public.html'), name='faq'),
     # path('contacts/', TemplateView.as_view(template_name='base/contacts_public.html'), name='contacts'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = 'srproj.main.views.resource_not_found'
 handler403 = 'srproj.main.views.forbidden'
