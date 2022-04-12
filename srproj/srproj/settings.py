68f3767ee5c261749351254c3557baffc6f5c618
iva <iva@borabora.localdomain>
2022-04-12 14:20:45 +0300

    db settings modified

--- srproj/srproj/settings.py
+++ srproj/srproj/settings.py
@@ -14,18 +14,19 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
-
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'hjchjschjdhcj788djhjvjcbvcnbnbc')
 DEBUG = False
 APP_ENV = os.getenv('APP_ENV')
 
-# if APP_ENV == 'Prod':
-#     ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
-# else:
-#     ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+if APP_ENV == 'Prod':
+    ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
+else:
+    ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
-ALLOWED_HOSTS = ['tech-service-request-app.herokuapp.com']
+# ALLOWED_HOSTS = ['tech-service-request-app.herokuapp.com']
 # Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -72,69 +73,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'srproj.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd4kjl67mnvdd26',
-        'USER': 'vspvksoaqudlgh',
-        'PASSWORD': 'b6de25d422eefb660e458388fa7718bcc350d13b4f43068a0975a415659cb0df',
-        'HOST': 'ec2-34-247-172-149.eu-west-1.compute.amazonaws.com',
-        'PORT': '5432',
-    }
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('DB_NAME', 'db.sqlite'),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', None),
+        'PORT': os.environ.get('DB_PORT', None),
+     }
 }
 
-# if APP_ENV == 'Prod':
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': os.getenv('DB_NAME'),
-#             'USER': os.getenv('DB_USER'),
-#             'PASSWORD': os.getenv('DB_PASSWORD'),
-#             'HOST': os.getenv('DB_HOST'),
-#             'PORT': os.getenv('DB_PORT', '5432'),
-#         }
-#     }
-# elif APP_ENV == 'Dev':
-#     # DATABASES = {
-#     #     'default': {
-#     #         'ENGINE': 'django.db.backends.postgresql',
-#     #         'NAME': os.getenv('DB_NAME'),
-#     #         'USER': os.getenv('DB_USER'),
-#     #         'PASSWORD': os.getenv('DB_PASSWORD'),
-#     #         'HOST': os.getenv('DB_HOST'),
-#     #         'PORT': os.getenv('DB_PORT'),
-#     #     }
-#     # }
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
+print(APP_ENV)
+print(SECRET_KEY)
+print(DATABASES)
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
-#     }
-# }
-# Password validation
-# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 if APP_ENV == 'Prod':
     AUTH_PASSWORD_VALIDATORS = [
